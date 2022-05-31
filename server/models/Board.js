d18bbd1c5caa37c7fc1ff1aa0c07bf4b90aa1129
Mason McCollum <mccollummason34@gmail.com>
2022-05-30 21:39:18 -0500

    5/30/22

---
+++ server/models/Board.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const boardSchema = new mongoose.Schema({
+  customer: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  crust: {
+    type: String,
+    required: true,
+    enum: []
+  },
+  cheese: {
+    type: String,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  sauce: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  toppings: [String]
+});
+
+const Board = mongoose.model("Board", boardSchema);
+module.exports = Board;
