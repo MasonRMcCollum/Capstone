5eea360d65b0d23e5ae57a470b4fc1c475ef86a9
Mason McCollum <mccollummason34@gmail.com>
2022-06-08 12:59:32 -0500

    6/8/22

--- components/views/Chat.js
+++ components/views/Chat.js
@@ -3,6 +3,7 @@ import html from "html-literal";
 export default state => html`
   <p>You are chatting with</p>
   <h1>${state.user}</h1>
+  <form id="chatview" method="Get"></form>
   <section id="Chatinput">
     <form id="Chatinput" method="POST" action="">
       <input type="text" />
