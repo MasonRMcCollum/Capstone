0729c10c896ccd7824e1ec17a3daa93af9455560
Mason McCollum <mccollummason34@gmail.com>
2022-06-23 15:47:12 -0500

    24

--- components/views/Profile.js
+++ components/views/Profile.js
@@ -4,5 +4,6 @@ export default state => html`
   <h1>${state.user}</h1>
   <div class="$1">
     <input type="button" value="Sign in/Sign out" />
+    <img src="https://i.redd.it/67u7dshy4vyz.jpg" />
   </div>
 `;
